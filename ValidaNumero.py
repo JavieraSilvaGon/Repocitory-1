5de727bdab13886b135a3654669321726eaa5116
JavieraSilvaGon <jaa.silvag@duocuc.cl>
2024-06-17 12:51:03 -0400

    validación de 2 números
    se agrega otra validación de numero, resultando ahora en el ingreso de dos números.

--- ValidaNumero.py
+++ ValidaNumero.py
@@ -7,3 +7,9 @@ while(True):
         break #quiebre o salgo de ciclo
     except ValueError:
         print("Debe ingresar un número!!!!")
+while(True):
+    try:
+        num=float(input("Ingrese un número  "))
+        break #quiebre o salgo de ciclo
+    except ValueError:
+        print("Debe ingresar un número!!!!")
