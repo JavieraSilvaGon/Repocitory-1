f4f3f8cb506e077024af3d0ef840e287ac5dfdd5
JavieraSilvaGon <jaa.silvag@duocuc.cl>
2024-06-17 12:42:45 -0400

    Add files via upload

---
+++ ValidaNumero.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls")
+
+while(True):
+    try:
+        num=float(input("Ingrese un número  "))
+        break #quiebre o salgo de ciclo
+    except ValueError:
+        print("Debe ingresar un número!!!!")
